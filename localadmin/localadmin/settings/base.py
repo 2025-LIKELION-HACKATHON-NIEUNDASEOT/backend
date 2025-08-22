b062680774cb97f71279bcaf91be7ceacb7a3d17
leewatertrue <leewatertrue@naver.com>
2025-08-22 23:33:53 +0900

    hotfix: villit 오타 수정

--- localadmin/localadmin/settings/base.py
+++ localadmin/localadmin/settings/base.py
@@ -88,7 +88,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOWED_ORIGINS = [
-    "https://vilit.vercel.app",
+    "https://villit.vercel.app",
     "http://localhost:3000",
 ]
 
